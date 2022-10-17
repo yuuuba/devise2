dd9b4a3d1f735e5a1503ee4b7d63e19770d953b5
yuuuba <nsabu45@gmail.com>
2022-10-17 17:18:49 +0900

    名前更新出来るところまで #4

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/show'
   root to: "pages#index"
   get 'pages/index'
   get 'pages/show'
+  get '/mypage', to: 'users#mypage'
   devise_for :users
+  resources :users, only: [:show, :edit, :update]
 end
